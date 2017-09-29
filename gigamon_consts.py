bd92669aa9330fb2d91fcf3560d61a2eb08732a8
phantomqa <david@phantom.us>
2017-09-29 14:03:47 -0700

    September 22, 2017 submission changes
    Phantom performed changes: Updated min phantom version and app version to 1.0.1

--- gigamon_consts.py
+++ gigamon_consts.py
@@ -2,11 +2,14 @@
 # File: gigamon_consts.py
 # --
 
+# FM specific
 GIGAMON_ERR_QUERY = "FM query failed"
 GIGAMON_SUCC_QUERY = "FM query successful"
 GIGAMON_ERR_QUERY_RETURNED_NO_DATA = "FM query did not return any information"
 GIGAMON_ERR_SERVER_CONNECTION = "Connection to server failed"
+# test connectivity action specific
 GIGAMON_ERR_CONNECTIVITY_TEST = "Connectivity test failed"
 GIGAMON_SUCC_CONNECTIVITY_TEST = "Connectivity test passed"
+# API specific
 GIGAMON_ERR_API_VERSION = "Incorrect API version. Please upgrade to v1.3"
 GIGAMON_API_VERSION = "v1.3"
